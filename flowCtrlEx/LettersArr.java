69a088cb172644595de66e616ba07704684afb67
Flexnm <58367828+Flexnm@users.noreply.github.com>
2019-12-28 15:53:21 +0200

    Update LettersArr.java

--- flowCtrlEx/LettersArr.java
+++ flowCtrlEx/LettersArr.java
@@ -5,17 +5,17 @@ import java.util.Arrays;
 public class LettersArr {
 	public static void main(String[] args) {
 		
-		String[] arr = {"O", "F", "I", "R", " ", "A", "R", "I", "E", "L", "I"};
+		String[] arr = {"B", "R", "U", "C", "E", " ", "W", "I", "L", "L", "I", "S"}; // Original array with a name.
 		
-		String[] arrRand = new String[arr.length];
+		String[] arrRand = new String[arr.length]; // The new array with a random order of letters.
 		
-		for (int i = 0; i < arr.length; i++) {
-			int a = (int)(Math.random()*arr.length);
-			if(arr[i] != null && arrRand[a] == null) {
-			arrRand[a] = arr[i];
-			arr[i] = null;
-			} else {
-				i--;
+		for (int i = 0; i < arr.length; i++) { // Running on the array arr.
+			int a = (int)(Math.random()*arr.length); // Randomizing a random index.
+			if(arr[i] != null && arrRand[a] == null) { // If there is a letter in the original array and free room in the new array.
+			arrRand[a] = arr[i]; // Put that letter in the new array in a random index.
+			arr[i] = null; // Delete that letter from the original array.
+			} else { // if the index in arr is empty or the random index of arrRand is full.
+				i--; // Randomize a new index for arrRand.
 			}
 		}
 		System.out.println(Arrays.toString(arrRand));
